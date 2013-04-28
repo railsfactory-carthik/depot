0e6f777be2ba44c4eb47f747880cde4c0551f244
karthik <railsfactory-carthik@example.com>
2013-04-28 21:57:16 +0530

    28-apr

---
+++ db/20130424121441_add_password_to_users.rb
@@ -0,0 +1,13 @@
+class AddPasswordToUsers < ActiveRecord::Migration
+ def self.up
+    add_column :users, :password, :string
+    add_column :users, :password_confirmation, :string
+ 
+  end
+  
+  def self.down
+    remove_column :users, :password
+    remove_column :users, :password_confirmation
+ 
+    end
+end
